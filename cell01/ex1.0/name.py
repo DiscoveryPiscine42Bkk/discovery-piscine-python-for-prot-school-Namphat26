e4334311488788dff377d6a76eeb3d1584af34b5
Namphat26 <petrada2612@gmail.com>
2025-03-10 12:13:35 +0700

    Add files via upload

---
+++ cell01/ex1.0/name.py
@@ -0,0 +1,10 @@
+# name.py
+
+# Define a variable for first name
+first_name = "Phatlada"
+
+# Define a variable for last name
+last_name = "Kamkhunthod"
+
+# Display the full name followed by a new line
+print(first_name, last_name)
