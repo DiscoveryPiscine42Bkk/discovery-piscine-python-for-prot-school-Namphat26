109b44ca9189cb5c0028db9a7e3b85f1ceda6db6
Namphat26 <petrada2612@gmail.com>
2025-03-10 13:54:32 +0700

    Add files via upload

---
+++ cell01/ex1.2/age.py
@@ -0,0 +1,10 @@
+# age.py
+
+# Define a variable for your age
+my_current_age = 30  # Replace this with your actual age
+
+# Add your age and 42, then assign the result to my_age
+my_age = my_current_age + 42
+
+# Display the my_age variable followed by a new line
+print(my_age)
