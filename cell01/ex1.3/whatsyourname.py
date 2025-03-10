a77d18e89ed19df880f261046f0dac9bc6f37ce7
Namphat26 <petrada2612@gmail.com>
2025-03-10 14:19:22 +0700

    Add files via upload

---
+++ cell01/ex1.3/whatsyourname.py
@@ -0,0 +1,10 @@
+# whatsyourname.py
+
+# Ask the user for their first name
+first_name = input("Please enter your first name: ")
+
+# Ask the user for their last name
+last_name = input("Please enter your last name: ")
+
+# Display the full name by concatenating first and last names
+print("Your full name is:", first_name, last_name)
