99a33a8086438dd348df7b3cbcb18df0bee3be73
Namphat26 <petrada2612@gmail.com>
2025-03-10 14:31:30 +0700

    Add files via upload

---
+++ cell02/ex.00/iszero.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Prompt the user to enter a number
+number = float(input("Please enter a number: "))
+
+# Check if the number is zero
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
