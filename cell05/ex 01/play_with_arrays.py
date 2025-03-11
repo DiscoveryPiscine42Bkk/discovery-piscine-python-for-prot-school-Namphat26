b01ddabf5f58d6141d881fd1efdc449df34e3926
Namphat26 <petrada2612@gmail.com>
2025-03-11 13:43:21 +0700

    Add files via upload

---
+++ cell05/ex 01/play_with_arrays.py
@@ -0,0 +1,5 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = [x + 2 for x in original_array]
+print("อาเรย์ต้นฉบับ:", original_array)
+print("อาเรย์ใหม่ที่เพิ่ม 2 ทุกค่าคือ:", new_array)
+
