6524148590f8ae173a08059a0e6b60e510ecd0b8
Namphat26 <petrada2612@gmail.com>
2025-03-13 14:27:03 +0700

    Add files via upload

---
+++ cell05/ex  08/aff_rev_params.py
@@ -0,0 +1,7 @@
+if len(sys.argv) == 3:
+    keyword = sys.argv[1]
+    text = sys.argv[2]
+    count = text.count(keyword)
+    print(count if count > 0 else "none")
+else:
+    print("none")  # Print "none" if the number of parameters is not 2
